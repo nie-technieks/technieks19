96dcb3c7e7485e03e25a481029e13265782431a0
mridulganga <mridul.kepler@gmail.com>
2018-02-05 23:47:10 +0530

    about done contacts and home needs work

--- technieks.py
+++ technieks.py
@@ -22,6 +22,14 @@ def events_all():
 # Here goes the code for the rest of the pages
 
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return "error"
