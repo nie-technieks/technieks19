427889fe6d673eac9a0e2384e2bd2bb1dd0d805d
Ram prasad E Naik <ramprasade10@gmail.com>
2019-02-04 21:47:15 +0530

    countdown js file added

--- technieks.py
+++ technieks.py
@@ -119,4 +119,4 @@ def page_not_found(e):
     return render_template("404.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080,debug=True)
